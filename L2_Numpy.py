a014dcca44c3e451d4f1c4d2aad917682d875c6d
iklieroon <rohanpras20@gmail.com>
2026-02-16 18:22:14 +0000

    Update L2_Numpy.py

--- L2_Numpy.py
+++ L2_Numpy.py
@@ -25,3 +25,11 @@ m2=np.random.permutation(16).reshape(4,4)
 print(m1)
 print(m2)
 print(m1+m2)
+
+#Create a function to solve an equasion: y=2x+3
+
+def solver(x):
+    return 2*x+3
+x1=np.array([1,2,3,4,5,6])
+y=solver(x1)
+print(y)
