311d1e546d11b9fd168ba4e1fc93f160ae0bfc5d
iklieroon <rohanpras20@gmail.com>
2026-05-25 18:48:58 +0100

    Add files via upload

--- L8_Machine_learning.py
+++ L8_Machine_learning.py
@@ -14,3 +14,32 @@ print(df.head())
 print(df.info())
 df['species']=df['species'].replace({'setosa':0,'versicolor':1,'virginica':2})
 print(df['species'])
+
+'''a=df['petal_width']
+b=df['petal_length']
+plt.scatter(a,b)
+plt.xlabel('petal width')
+plt.ylabel('petal length')
+plt.title('petal')
+plt.show()
+
+c=df['sepal_width']
+d=df['sepal_length']
+plt.scatter(a,b)
+plt.xlabel('sepal width')
+plt.ylabel('sepal length')
+plt.title('sepal')
+plt.show()
+'''
+x=df.drop('species',axis=1)
+y=df['species']
+print(x.head())
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1)
+print(x_train.shape)
+print(x_test.shape)
+
+model=DecisionTreeClassifier(max_depth=3,random_state=1)
+model.fit(x_train,y_train)
+predictions=model.predict(x_test)
+print('accuracy',metrics.accuracy_score(predictions,y_test))
